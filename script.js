0620c37e5c5bc7975b6169c0ea32245835f06b5c
Toshika-02 <105552050+Toshika-02@users.noreply.github.com>
2024-01-15 19:21:51 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,18 @@
 function firstWord(s) {
   // your code here
+	const str = '';
+	for(const word of s){
+		if(s.length===1){
+			return str;
+		}
+	}
+	  return s;
+	
+	
+	
 }
 
 // Do not change the code below
 
-const s = prompt("Enter String:");
-alert(firstWord(s));
+// const s = prompt("Enter String:");
+// alert(firstWord(s));
